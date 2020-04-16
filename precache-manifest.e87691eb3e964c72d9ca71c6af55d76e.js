73824566b9b062f2f2ba692be08f5beaffcc9958
Prasanna L <prasana24n97@gmail.com>
2020-04-16 15:32:24 +0530

    Updates

---
+++ precache-manifest.e87691eb3e964c72d9ca71c6af55d76e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efbd15e9edb8df8119143c9876fd144",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3b9c2e1b0b74c9eec51c",
+    "url": "/covid-19-tracker/static/js/2.2a69806a.chunk.js"
+  },
+  {
+    "revision": "730ffd10f627bccad7f26349c756e4a4",
+    "url": "/covid-19-tracker/static/js/2.2a69806a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611be02d54033e405f6b",
+    "url": "/covid-19-tracker/static/js/main.62e22a7e.chunk.js"
+  },
+  {
+    "revision": "24cadf4b12e69ae7572e",
+    "url": "/covid-19-tracker/static/js/runtime-main.2d18d3bc.js"
+  }
+]);
